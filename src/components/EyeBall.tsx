e67b3040e2854a41b10927cbc393fa3a71d6013e
EncryptedSam <sampathkumar.ajjapaga@gmail.com>
2025-04-19 07:33:41 +0530

    deploy settings

--- src/components/EyeBall.tsx
+++ src/components/EyeBall.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import { useRef } from 'react'
 import useMousePosition from '../hooks/useMousePosition';
 import { getIntermediatePoint } from '../utils/getIntermediatePoint';
 
